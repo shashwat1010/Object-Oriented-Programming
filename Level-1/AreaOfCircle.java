5a2f5501983d9c6bb75288b488e6e123ddcceb8d
Shashwat Rana <126244043+shashwat1010@users.noreply.github.com>
2025-08-13 12:28:26 +0530

    Create AreaOfCircle.java

---
+++ Level-1/AreaOfCircle.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Circle {
+    double radius;
+
+    // Constructor
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    // Method to calculate area
+    double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    // Method to calculate circumference
+    double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    // Method to display details
+    void displayDetails() {
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + calculateArea());
+        System.out.println("Circumference: " + calculateCircumference());
+    }
+}
+
+public class CircleDetails {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius of the circle: ");
+        double radius = sc.nextDouble();
+
+        Circle circle = new Circle(radius);
+        circle.displayDetails();
+    }
+}

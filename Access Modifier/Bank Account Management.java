4a9f7b3ff88c76e8539da227dbec463da6a4779c
Shashwat Rana <126244043+shashwat1010@users.noreply.github.com>
2025-08-15 11:48:22 +0530

    Create Bank Account Management.java

---
+++ Access Modifier/Bank Account Management.java
@@ -0,0 +1,49 @@
+// Base Class
+public class BankAccount {
+    public String accountNumber;
+    protected String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    // Public getter & setter for balance
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient funds!");
+        }
+    }
+}
+
+// Subclass
+class SavingsAccount extends BankAccount {
+    public SavingsAccount(String accountNumber, String accountHolder, double balance) {
+        super(accountNumber, accountHolder, balance);
+    }
+
+    public void displayAccountDetails() {
+        // Can access public accountNumber
+        System.out.println("Account Number: " + accountNumber);
+        // Can access protected accountHolder
+        System.out.println("Account Holder: " + accountHolder);
+        // Cannot access private balance directly
+        System.out.println("Balance: " + getBalance());
+    }
+}
